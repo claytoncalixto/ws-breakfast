05e56a540dc15a7d2312eb9e672989b18aea6ef3
Clayton Calixto <claytonrcalixto@gmail.com>
2021-11-15 16:17:56 -0300

    Created DataTable

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/claytoncalixto" target="_blank" rel="noreferrer">Clayton Roberto Calixto</a></p>
+                <p className="text-light"><small><strong>Semana Desafio Unidac - Grupo WL</strong><br />
+      Evento do Desafio Unidac - Grupo WL: <a href="https://github.com/unidac-grupo-wl/desafio-unidac-grupo-wl" target="_blank" rel="noreferrer">@wlgrupo</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
